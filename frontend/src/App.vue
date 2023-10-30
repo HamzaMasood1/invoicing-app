c0e7677275dba3c6a3329ce209b46217e72b4b90
HamzaMasood1 <hamzamasood183@gmail.com>
2023-10-30 00:48:25 +0000

    added a test function in golang to use in vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import HelloWorld from './components/HelloWorld.vue'
-import Navbar from "./components/Navbar.vue";</script>
+import Navbar from "./components/Navbar.vue";
+import Test from "./components/test.vue";</script>
 
 <template>
   <HelloWorld/>
   <Navbar/>
+  <test/>
 </template>
 
