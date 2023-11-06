f964eb721228245300035168e123b282e72d0561
HamzaMasood1 <hamzamasood183@gmail.com>
2023-11-06 00:20:50 +0000

    removed component library

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,8 @@
 <script lang="ts" setup>
 import HelloWorld from './components/HelloWorld.vue'
-import Navbar from "./components/Navbar.vue";
 import Test from "./components/test.vue";</script>
 
 <template>
   <HelloWorld/>
-  <Navbar/>
-  <test/>
 </template>
 
